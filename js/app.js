8112a5c0df2f98155e80ae45fc441b3bc6941cc7
vinyasbj <vinyas555@gmail.com>
2018-05-19 22:46:21 +0530

    particleJS changes

--- js/app.js
+++ js/app.js
@@ -23,13 +23,13 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#29FCA5"
       },
       "shape": {
         "type": "circle",
         "stroke": {
           "width": 0,
-          "color": "#000000"
+          "color": "#29FCA5"
         },
         "polygon": {
           "nb_sides": 5
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#29FCA5",
         "opacity": 0.4,
         "width": 1
       },
